0497c62a5942aaae0e642d74e5e8f34d70787956
Yalda Foroutan <yoolenda@Yaldas-MacBook-Pro-6.local>
2025-03-06 00:47:28 -0800

    update education

--- src/data/education.ts
+++ src/data/education.ts
@@ -12,14 +12,30 @@ export const educationData: Education[] = [
   {
     year: "2024—Present",
     institution: "Simon Fraser University (SFU)",
-    degree: "Ph.D. in Computing Science",
-    advisor: "Prof. Andrea Tagliasacchi",
+    degree: "Ph.D. in Computer Science",
+    // advisor: "Prof. Andrea Tagliasacchi",
   },
   {
-    year: "2017—2021",
-    institution: "Massachusetts Institute of Technology",
-    degree: "B.S. in Computer Science and Mathematics",
-    thesis: "Algorithmic Approaches to Causal Discovery",
+    year: "2021—2023",
+    institution: "Simon Fraser University (SFU)",
+    degree: "Ph.D. in Engineering Science",
+    // thesis: "Algorithmic Approaches to Causal Discovery",
+    // Optional links to thesis
+    // thesisUrl: "https://dspace.mit.edu/handle/1721.1/149111"
+  },
+  {
+    year: "2017—2020",
+    institution: "University of Tehran",
+    degree: "M.Sc. in Electrical Engineering",
+    thesis: "Computer Mouse Control via Hand Gesture Recognition",
+    // Optional links to thesis
+    // thesisUrl: "https://dspace.mit.edu/handle/1721.1/149111"
+  },
+  {
+    year: "2012—2017",
+    institution: "Amirkabir University of Technology (Tehran Polytechnic)",
+    degree: "B.Sc. in Electrical Engineering (Control)",
+    thesis: "Analyzing Driver's Consciousness Levels Using EEG Signals",
     // Optional links to thesis
     // thesisUrl: "https://dspace.mit.edu/handle/1721.1/149111"
   },
